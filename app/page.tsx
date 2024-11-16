4cec8712b56e8fc070536af203f457b5b9ee33db
GChamindu <gavishka.rightmo@gmail.com>
2024-11-16 21:29:02 +0530

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return <main>
 
 
-    <div>
+    <div className="relative bg-black-100 text-white flex items-center justify-center flex-col overflow-hidden ">
 
 
       <h1>dad</h1>
