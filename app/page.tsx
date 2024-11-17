baf86abd9142293615dd68061267165dbc7dbfe8
GChamindu <gavishka.rightmo@gmail.com>
2024-11-17 15:52:27 +0530

    use Aceternity UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="relative bg-black-100 text-white flex items-center justify-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-
-
-    <div className=" ">
-
-
-      <h1>dad</h1>
-    </div>
-  </main>;
+  return (
+    <main className="relative bg-black-100 text-white flex items-center justify-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl  w-full">
+        <Hero />
+      </div>
+    </main>
+  );
 }
