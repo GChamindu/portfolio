33bb98835264c9736508c6177b5313e49423d0bf
GChamindu <gavishka.rightmo@gmail.com>
2024-11-17 15:20:22 +0530

    START hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>
+  return <main className="relative bg-black-100 text-white flex items-center justify-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
 
 
-    <div className="relative bg-black-100 text-white flex items-center justify-center flex-col overflow-hidden ">
+    <div className=" ">
 
 
       <h1>dad</h1>
