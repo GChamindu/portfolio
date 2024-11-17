dd3cb531f25f68db8c1681435e1bfbf6e020d29b
GChamindu <gavishka.rightmo@gmail.com>
2024-11-17 19:49:37 +0530

    ..

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div></div>
+      <div>
+        <Spotlight className="-top-40 -left-10 md:-left-32md:-top-20 "/>
+      </div>
     </div>
   );
 };
